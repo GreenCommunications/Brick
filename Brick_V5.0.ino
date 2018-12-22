3270956e6a10016f04a0f09a87fd13636786d0e9
Alex R <alexander.m.rankin@gmail.com>
2018-12-22 13:44:48 -0500

    Add files via upload

---
+++ Brick_V5.0.ino
@@ -0,0 +1,17 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
+
+void setup() {
+  analogWrite(10, 255);
+  lcd.begin(16, 2);
+  
+  lcd.print("BrickOS");
+  lcd.setCursor(0, 2);
+  lcd.print("V5.0");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
